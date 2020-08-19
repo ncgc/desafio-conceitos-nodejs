82dc5e13d4d6410ddbf87beec495fb7c1044f8ff
Natalia C G <natalia.datascience@gmail.com>
2020-08-19 00:09:36 -0300

    first answer by ncgc

--- src/app.js
+++ src/app.js
@@ -1,33 +1,85 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-
 const repositories = [];
 
+function findRepository(id){
+  return repositories.find(repository => repository.id == id);
+}
+
+function validateId(request, response, next){
+  const { id } = request.params;
+
+  if (!findRepository(id)){
+    return response.status(400).json({error: 'Not found'});
+  }
+
+  return next();
+};
+
+
+app.use(express.json());
+app.use(cors());
+app.use('/repositories/:id', validateId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes:0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+ 
+  repository = findRepository(id);
+  const likes = repository.likes;
+
+  repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes,
+  };
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  repositories.splice(repositories[id]);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like",validateId, (request, response) => {
+  const { id } = request.params;
+
+  repository = findRepository(id);
+  new_likes = repository.likes + 1;
+  repository.likes = new_likes;
+
+  return response.json(repository);
 });
 
 module.exports = app;
